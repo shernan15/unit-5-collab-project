2d86c85778ccf3ebfce2107ceb5256d7d286e28d
yoyoyogabagaba <96128094+yoyoyogabagaba@users.noreply.github.com>
2021-12-14 09:28:57 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,48 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     let Snow = 0
     Question = game.askForString("Did it snow yesterday?", 3)
     if (Snow == Yes) {
-    	
+        callAll()
+        pause(1000)
+        callAll()
     } else {
     	
     }
 })
+function snow () {
+    for (let index = 0; index < 10; index++) {
+        projectile2 = sprites.createProjectileFromSide(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . 1 . . . . . . . . . . 
+            . . 1 1 . . . 1 1 1 . . . . . . 
+            . . 1 1 . 1 1 1 1 1 1 1 . . . . 
+            . . . . 1 1 1 1 1 1 1 1 1 . . . 
+            . . . 1 1 1 1 1 1 1 1 1 1 1 . . 
+            . . . 1 1 1 1 1 1 1 1 1 1 1 . . 
+            . . 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+            . . 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+            . . 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+            . . . 1 1 1 1 1 1 1 1 1 1 1 . . 
+            . . . 1 1 1 1 1 1 1 1 1 1 1 . . 
+            . . . . 1 1 1 1 1 1 1 1 1 . . . 
+            . . 1 . . 1 1 1 1 1 1 1 . . . . 
+            . . . . . . . 1 1 1 . . . . 1 . 
+            . . . . . . . . . . . . . . . . 
+            `, randint(0, -50), randint(0, 50))
+    }
+}
+function callAll () {
+    snow()
+    pause(1000)
+    snow()
+    pause(1000)
+    snow()
+    pause(1000)
+    snow()
+    pause(1000)
+    snow()
+    pause(1000)
+}
+let projectile2: Sprite = null
 let Question = ""
 scene.setBackgroundColor(3)
 let Cat = sprites.create(assets.image`Cat`, SpriteKind.Player)
