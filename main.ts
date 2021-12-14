c83699300e1df61a27935eec642041e44801414a
shernan15 <96128015+shernan15@users.noreply.github.com>
2021-12-14 09:17:51 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    let Yes = 0
+    let Snow = 0
+    Question = game.askForString("Did it snow yesterday?", 3)
+    if (Snow == Yes) {
+    	
+    } else {
+    	
+    }
+})
+let Question = ""
 scene.setBackgroundColor(3)
 let Cat = sprites.create(assets.image`Cat`, SpriteKind.Player)
 Cat.setPosition(77, 97)
-Cat.sayText(game.askForString("Did it snow yesterday?", 3), 1000, false)
+Cat.sayText("Did it snow yesterday? Press B to answer", 5000, false)
