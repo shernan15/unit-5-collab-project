58dc57283eeccca2d101d099f94f962b3a7a9924
shernan15 <96128015+shernan15@users.noreply.github.com>
2021-12-14 09:05:45 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
 scene.setBackgroundColor(3)
 let Cat = sprites.create(assets.image`Cat`, SpriteKind.Player)
+Cat.setPosition(77, 97)
+Cat.sayText(game.askForString("Did it snow yesterday?", 3), 1000, false)
